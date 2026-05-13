efaff9c47e3f301529b82039a496517f918e6bd1
Gurami <studettholiday@gmail.com>
2026-05-13 22:55:50 +0400

    serve client from express

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const chatRouter = require('./routes/chat');
 
 const app = express();
@@ -10,8 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/chat', chatRouter);
-
 app.get('/health', (_req, res) => res.json({ status: 'ok' }));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
